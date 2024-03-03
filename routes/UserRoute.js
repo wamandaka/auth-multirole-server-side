b5b572f4933e59f5f02d56f153d56bf6a3723d2b
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-03-03 17:07:11 +0700

    penyesuaian

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -4,15 +4,15 @@ import {
   deleteUser,
   getUserById,
   getUsers,
-  updateUser,
+  updateUserById,
 } from "../controllers/Users.js";
 import { verifyUser, adminOnly } from "../middleware/AuthUser.js";
 const router = express.Router();
 
 router.get("/users", verifyUser, adminOnly, getUsers);
 router.get("/users/:id", verifyUser, adminOnly, getUserById);
-router.post("/users", verifyUser, adminOnly, createUser);
-router.patch("/users/:id", verifyUser, adminOnly, updateUser);
+router.post("/users", createUser);
+router.patch("/users/:id", verifyUser, adminOnly, updateUserById);
 router.delete("/users/:id", verifyUser, adminOnly, deleteUser);
 
 export default router;
